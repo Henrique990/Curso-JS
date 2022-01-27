95f6723c38c576e558a9634415d60e5f1b622bbd
Henrique Teixeira <henriqueteixeira26@gmail.com>
2022-01-27 12:06:48 -0300

    exercicio 1

---
+++ operaçõesAritméticas/01.js
@@ -0,0 +1,61 @@
+// + Adição
+// - Subtração
+// * Multiplicação
+// / Divisão
+// % Módulo
+// ++ Incremento
+// -- Decremento
+
+// Exemplo 1: Adição
+var a = 10;
+var b = 5;
+var c = a + b;
+console.log(c);
+// Resultado: 15
+
+// Exemplo 2:Subtração
+var a = 10;
+var b = 5;
+var c = a - b;
+console.log(c);
+// Resultado: 5
+
+// Exemplo 3: Multiplicação
+var a = 10; 
+var b = 5;
+var c = a * b;
+console.log(c);
+// Resultado: 50
+
+// Exemplo 4: Divisão
+var a = 10;
+var b = 5;
+var c = a / b;
+console.log(c);
+// Resultado: 2
+
+// Exemplo 5: Módulo
+var a = 10;
+var b = 5;
+var c = a % b;
+console.log(c);
+// Resultado: 0   (10 % 5 = 0)
+
+// Exemplo 6: Incremento
+var a = 10;
+++a;
+var b = 5;
+++b;
+var c = a + b;
+console.log(c);
+// Resultado: 17
+
+// Exemplo 7: Decremento
+var a = 10;
+--a;
+var b = 5;
+--b;
+var c = a + b;
+console.log(c);
+// Resultado: 13
+
