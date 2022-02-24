394b4baca9215c190972dc21000f7caba7399a5b
Henrique Teixeira <henriqueteixeira26@gmail.com>
2022-02-24 11:43:35 -0300

    Entendendo a impoortancia de uma constante

--- Variaveis e Tipos/constante.js
+++ Variaveis e Tipos/constante.js
@@ -1,1 +1,8 @@
-const FIRST_NAME = "Stephany";
+const FIRST_NAME = "Stephany"; // uma constante não se altera durante todo o codigo
+
+// não é possivel reatribuir o valor de FIRST_NAME, 
+//FIRST_NAME = "Julia"; //TypeError: Assignment to constant variable. Esse codigo deu erro pois não é possivel alterar uma constante
+
+
+//Tambem não é possivel redeclarar uma constante, como vemos no exemplo a seguir
+const FIRST_NAME = "Julia"; //SyntaxError: Identifier 'FIRST_NAME' has already been declared
