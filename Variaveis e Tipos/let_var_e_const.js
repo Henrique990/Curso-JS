a6f853455af34d810d25a3e9449cf5df7145ac03
Henrique Teixeira <henriqueteixeira26@gmail.com>
2022-02-24 11:57:15 -0300

    explicando a diferença entre var, let e const

---
+++ Variaveis e Tipos/let_var_e_const.js
@@ -0,0 +1,8 @@
+// VAR...LET...CONST
+
+
+// VAR: Escopo global ou local, pode ser redeclarado, pode ser reatribuido e pode ter o hoisting.
+
+// CONST: Tem escopo de Bloco, não pode ser redeclarado, não pode ser reatribuido e não é possivel usar o hoisting.
+
+// LET: Tem escopo de Bloco, não pode ser redeclarado, pode ser reatribuido e não é possivem utilizar o hoisting.
