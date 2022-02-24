05a60eade512b93f31a68026fa8f44c3ce736206
Henrique Teixeira <henriqueteixeira26@gmail.com>
2022-02-24 16:49:09 -0300

    aprendi um pouco sobre estrutura de dados

---
+++ Variaveis e Tipos/Tipos/estrutura_de_dados.js
@@ -0,0 +1,8 @@
+let numero = 1; 
+let texto = "um"; 
+let falso = false 
+
+
+console.log(typeof texto) // resultado: String
+console.log(typeof numero) // resultado: Number
+console.log(typeof falso) // resultado: Boolean
