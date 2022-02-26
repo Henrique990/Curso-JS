1622086add9f349ef62bf9f9e49ec3a2e44c59a3
Henrique Teixeira <henriqueteixeira26@gmail.com>
2022-02-26 16:52:16 -0300

    aprendendo sobre valores Booleanos

---
+++ Variaveis e Tipos/Tipos/booleans.js
@@ -0,0 +1,15 @@
+// Valores Booleanos retorna sempre True ou false
+
+let validation = 3 === 0; // 3 é igual a 0?
+console.log(validation); // resultado: false
+
+validation = 3 === 3;
+console.log(validation); // resultado: true
+
+
+validation = 3 > 4;
+console.log(validation); // resultado: false
+
+console.log(!validation) // resultado: true
+                         // a "!" retorna o valor contrario
+
