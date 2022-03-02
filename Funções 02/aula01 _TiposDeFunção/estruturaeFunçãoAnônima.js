7f6f82d9cd51c6027bce23588ea917ab4dd31600
Henrique Teixeira <henriqueteixeira26@gmail.com>
2022-03-02 16:43:10 -0300

    funçoes anonimas

---
+++ Funções 02/aula01 _TiposDeFunção/estruturaeFunçãoAnônima.js
@@ -0,0 +1,16 @@
+
+// CRIANDO UMA FUNÇÃO ANONIMA
+const soma = function (a, b) {
+    return a + b;
+}
+console.log(`O resultado é ${soma(1, 10)}`);
+    
+const soma1 = function (y, z) {
+    return y * z;
+}
+console.log(`O resultado é ${soma1(5, 5)}`)
+
+const soma2 = function (a, b, c, d) {
+    return a + b + c + d;
+}
+console.log(`O resultado é ${soma2("henrique ","teixeira ","oliveira ","carvalho")}`)
